01b1a5268633a965960524f5a005c4f59a1b70be
Yshwartz <yshwartz@tremorvideodsp.com>
2019-02-21 21:35:13 +0200

    adding filters

--- src/app/filters-row/filters-row.component.ts
+++ src/app/filters-row/filters-row.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
 
 @Component({
-  selector: 'filters-row',
-  templateUrl: './filters-row.component.html',
-  styleUrls: ['./filters-row.component.css']
+  selector: "filters-row",
+  templateUrl: "./filters-row.component.html",
+  styleUrls: ["./filters-row.component.css"]
 })
 export class FiltersRowComponent implements OnInit {
+  @ViewChild("picker") picker;
+  @ViewChild("picker2") picker2;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  onselect() {
+    console.log(picker);
   }
-
 }
